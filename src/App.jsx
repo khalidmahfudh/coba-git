1fcaf2b959030a8b9b4bc29d32fbe3e2b0758c19
khalid mahfudh al azizi <khalidmahfudh94@gmail.com>
2024-01-26 16:34:26 +0700

    Memisahkan data Mahasiswa menjadi komponen Mahasiswa dan Menambahkan beberapa git command di git-command.txt

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import Dosen from "./components/Dosen";
+import Mahasiswa from "./components/Mahasiswa";
 
 function App() {
   return <div className="Container">
-    <h3>Data Mahasiswa</h3>
-    <ul>
-      <li>Khalid Mahfudh : 150504044</li>
-      <li>John Doe : 150504064</li>
-      <li>Balmond : 150504012</li>
-    </ul>
+    <Mahasiswa/>
     <Dosen/>
   </div>;
 }
