5c03be7477f1bb2c0aeb812e184d8152775ee699
khalid mahfudh al azizi <khalidmahfudh94@gmail.com>
2024-01-26 12:02:17 +0700

    Menambahkan data mahasiswa di App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return;
+  return <div className="Container">
+    <h3>Data Mahasiswa</h3>
+    <ul>
+      <li>Khalid Mahfudh : 150504044</li>
+      <li>John Doe : 150504064</li>
+    </ul>
+  </div>;
 }
 
 export default App;
